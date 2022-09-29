711d626638a274229db9c7b78f0ed9236b5d228e
ignaciosanabria <ignaciosanabria@outlook.com>
2022-09-29 13:42:29 -0300

    Entrega segundo desafio lista alumnos

--- src/app/components/student/student.component.ts
+++ src/app/components/student/student.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Persona } from 'src/app/models/persona';
 
 @Component({
   selector: 'app-student',
@@ -8,6 +9,19 @@ import { Component, OnInit } from '@angular/core';
 export class StudentComponent implements OnInit {
 
   constructor() { }
+
+  listaAlumnos: Array<Persona> = [
+    {nombre: 'Lautaro',apellido: 'Martinez', edad: 25},
+    {nombre: 'Emiliano',apellido: 'Martinez', edad: 30},
+    {nombre: 'Lionel',apellido: 'Messi', edad: 35},
+    {nombre: 'Rodrigo',apellido: 'De Paul', edad: 28},
+    {nombre: 'Angel',apellido: 'Di Maria', edad: 34},
+    {nombre: 'Paulo',apellido: 'Dybala', edad: 28},
+    {nombre: 'Alejandro',apellido: 'Gomez', edad: 34},
+    {nombre: 'Joaquin',apellido: 'Correa', edad: 28},
+    {nombre: 'Julian', apellido:'Alvarez', edad: 22},
+    {nombre: 'Nicolas', apellido:'Otamendi',edad:34}
+  ];
 
   ngOnInit(): void {
   }
